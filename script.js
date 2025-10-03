996ba04a599968777a7a22cf6be66ae13d56cb2c
Suwannaphoom <68070193@kmitl.ac.th>
2025-10-03 12:52:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const myinput = document.getElementById("myinput")
+
+function add() {
+    myinput.value = Number(myinput.value) + 1
+}
+function rob() {
+    myinput.value = Number(myinput.value) - 1
+}
