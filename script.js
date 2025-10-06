27a86cc0b31456aa0028686f19445db6b7046cba
Suwannaphoom <68070193@kmitl.ac.th>
2025-10-06 20:42:03 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const myinput = document.getElementById("myinput")
-
-function add() {
-    myinput.value = Number(myinput.value) + 1
-}
-function rob() {
-    myinput.value = Number(myinput.value) - 1
-}
